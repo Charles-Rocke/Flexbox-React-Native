2ea3866bc6b0db05a22a76ea4c4086d424bbfedb
SpOOk <spook.devv@gmail.com>
2023-08-14 08:20:26 -0400

    Learning Flexbox in React Native

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={{
+            padding: 50,
+            width: "100%",
+            height: 300,
+            justifyContent: "space-around",
+            alignItems: "stretch"
+            }}>
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: 'red',
+                    justifyContent: 'center',
+                    alignItems: 'stretch',
+                }}>
+                <Text>1</Text>
+            </View>
+            <View
+                style={{
+                    flex: 2,
+                    backgroundColor: 'blue',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Text>2</Text>
+            </View>
+            <View
+                style={{
+                    backgroundColor: 'green',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Text>3</Text>
+            </View>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
